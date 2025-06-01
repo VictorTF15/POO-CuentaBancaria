fa6ec6352d170e224ec235e3e768e1b59be47455
VictorTF15 <utp0210623@alumno.utpuebla.edu.mx>
2025-05-31 21:34:04 -0600

    Add files via upload

---
+++ CuentaBancaria.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuentaBancaria
+{
+    internal class CuentaBancaria
+    {
+        private string titular;
+        private double saldo;
+
+
+        public CuentaBancaria(string titular, double saldoInicial)
+        {
+           this.titular = titular;
+           saldo = saldoInicial;
+        }
+
+
+        public string Depositar(double cantidad)
+        {
+            if (cantidad > 0)
+            {
+                saldo += cantidad;
+                return $"Depósito exitoso: ${cantidad}. Nuevo saldo: ${saldo}";
+            }
+            else
+            {
+                return "La cantidad a depositar debe ser mayor que 0.";
+            }
+        }
+
+
+        public string Retirar(double cantidad)
+        {
+            if (cantidad > 0 && cantidad <= saldo)
+            {
+                saldo -= cantidad;
+                return $"Retiro exitoso: ${cantidad}. Nuevo saldo: ${saldo}";
+            }
+            else
+            {
+                return "Fondos insuficientes o cantidad inválida.";
+            }
+        }
+
+
+        public double ConsultarSaldo()
+        {
+            return saldo;
+        }
+
+
+        public string MostrarInformacion()
+        {
+            return $"Titular: {titular} - Saldo actual: ${saldo}";
+        }
+
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CuentaBancaria
+{
+    public partial class Form1 : Form
+    {
+        private CuentaBancaria cuenta;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            string titular = textBox1.Text;
+            double saldoInicial = double.Parse(textBox2.Text);
+            cuenta = new CuentaBancaria(titular, saldoInicial);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(cuenta.Depositar(double.Parse(textBox3.Text)));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(cuenta.MostrarInformacion());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(cuenta.Retirar(double.Parse(textBox4.Text)));
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            double saldoActual = cuenta.ConsultarSaldo();
+            MessageBox.Show($"Saldo final: ${saldoActual}");
+        }
+    }
+}
